78bd14d50138b3cffeb2ad293dc7bfa92bd5618b
Mercury-2910 <bao.dang2910022hcmut.edu.vn>
2023-07-10 10:04:57 +0700

    first commit

--- src/pages/XPage_Login/XPage_Login.jsx
+++ src/pages/XPage_Login/XPage_Login.jsx
@@ -1,8 +1,11 @@
-import { Typography } from "@mui/material"
+import { Typography, Button } from "@mui/material"
 
 const XPage_Login = () => {
     return (
-        <Typography variant="body1">XPage_Login</Typography>
+        <>
+            <Typography variant="body1">XPage_Login</Typography>
+            <Button>Click</Button>
+        </>
     )
 }
 
