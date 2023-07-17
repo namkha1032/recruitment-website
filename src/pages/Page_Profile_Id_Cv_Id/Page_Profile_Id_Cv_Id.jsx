4bfe6bc3245f85bd7d77133dcbd52a0bb3642183
Hoang <hoang.doan2408.bk@hcmut.edu.vn>
2023-07-18 01:31:56 +0700

    update

--- src/pages/Page_Profile_Id_Cv_Id/Page_Profile_Id_Cv_Id.jsx
+++ src/pages/Page_Profile_Id_Cv_Id/Page_Profile_Id_Cv_Id.jsx
@@ -6,10 +6,12 @@ const Page_Profile_Id_Cv_Id = () => {
   const navigate = useNavigate()
   return (
     <>
-      <Box sx={{ border: "1px solid black", padding: 10, borderRadius: 4 }}>
+      <Box sx={{ border: "1px solid black", padding: 10, borderRadius: 4 ,mb:5}}>
         <CV />
       </Box>
-      <Button variant="contained" color="warning" onClick={() => navigate("/profile/1/cv/1/update")}>update</Button>
+      <Box sx={{display:'flex',justifyContent:'flex-end'}}>
+            <Button  variant="contained" color="warning" onClick={() => navigate("/profile/1/cv/1/update")}>update</Button>
+      </Box>
     </>
   )
 }
