2b48320d2f591672b8db10d9d944ae842852edd6
Thắng Nguyễn Việt <vietthangpm598@gmail.com>
2023-07-19 14:03:15 +0700

    Add div-box

--- src/pages/Page_Profile_CvCreate/Child/CvData.jsx
+++ src/pages/Page_Profile_CvCreate/Child/CvData.jsx
@@ -1,31 +1,12 @@
 import dayjs from "dayjs";
 const cvinfo = {
-  title: "New Cv",
-  intro: "sdasdasd",
-  certificates: [
-    {
-      id: 0,
-      name: "IELTS",
-      organize: "British Council",
-      startDate: dayjs("2022-04-17"),
-      endDate: null,
-      detail: "dasdasdasd",
-      link: "dadadadadadasd",
-    },
-  ],
-  skills: [{ id: 0, name: "html" }],
-  language: [
-    { id: 0, languageId: 0, languageName: "English" },
-    { id: 1, languageId: 1, languageName: "Japanese" },
-  ],
-  education: "ada",
-  experience: "dasdad",
+  title: "",
+  intro: "",
+  certificates: [],
+  skills: [],
+  language: [],
+  education: "",
+  experience: "",
 };
-// export const certificateList = [
-//   { name: "IELTS" },
-//   { name: "TOEIC" },
-//   { name: "python" },
-//   { name: "javaScript" },
-// ];
 export const steps = ["Cv Infor", "Yout Experience", "Yout Certificate"];
 export default cvinfo;
