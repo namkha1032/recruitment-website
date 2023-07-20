ef2717d917815ad33b625a91cc383e93933ce9ca
HuyWithNY <huyhmopubg@gmail.com>
2023-07-20 09:57:12 +0700

    edit lan 2

--- src/pages/Page_Profile_Id_Application/Page_Profile_Id_Application.jsx
+++ src/pages/Page_Profile_Id_Application/Page_Profile_Id_Application.jsx
@@ -5,46 +5,57 @@ export default function Page_Profile_Id_Application(){
         {
           id: 1,
           name: 'Application 1',
+          status: 'Kết thúc',
         },
         {
           id: 2,
           name: 'Application 2',
+          status: 'Sắp diễn ra',
         },
         {
           id: 3,
           name: 'Application 3',
+          status: 'Sắp diễn ra',
         },
         {
           id: 4,
           name: 'Application 4',
+          status: 'Sắp diễn ra',
         },
         {
           id: 5,
           name: 'Application 5',
+          status: 'Kết thúc',
         },
         {
           id: 6,
           name: 'Application 6',
+          status: 'Kết thúc',
         },
         {
           id: 7,
           name: 'Application 7',
+          status: 'Kết thúc',
         },
         {
           id: 8,
           name: 'Application 8',
+          status: 'Kết thúc',
         },
         {
           id: 9,
           name: 'Application 9',
+          status: 'Kết thúc',
         },
         {
           id: 10,
           name: 'Application 10',
+          status: 'Kết thúc',
         },
         {
           id: 11,
           name: 'Application 11',
+          status: 'Kết thúc',
         },
         // Thêm các dữ liệu cho các event khác
       ];
@@ -54,7 +65,7 @@ export default function Page_Profile_Id_Application(){
       const pathnavigate = '/recruitment/:recruitmentid/application/1';
       const namePage = 'Position name';
       return(
-        <HistoryList events={eventsData} time={time} status={status} itemsPerPage={itemsPerPage} pathnavigate={pathnavigate} namePage={namePage} />
+        <HistoryList events={eventsData} time={time} itemsPerPage={itemsPerPage} pathnavigate={pathnavigate} namePage={namePage} />
       )
       
 }
--- src/pages/Page_Profile_Id_Interview/Page_Profile_Id_Interview.jsx
+++ src/pages/Page_Profile_Id_Interview/Page_Profile_Id_Interview.jsx
@@ -5,56 +5,67 @@ export default function Page_Profile_Id_Interview(){
         {
           id: 1,
           name: 'Interview 1',
+          status: 'Kết thúc',
         },
         {
           id: 2,
           name: 'Interview 2',
+          status: 'Kết thúc',
         },
         {
           id: 3,
           name: 'Interview 3',
+          status: 'Kết thúc',
         },
         {
           id: 4,
           name: 'Interview 4',
+          status: 'Kết thúc',
         },
         {
           id: 5,
           name: 'Interview 5',
+          status: 'Kết thúc',
         },
         {
           id: 6,
           name: 'Interview 6',
+          status: 'Kết thúc',
         },
         {
           id: 7,
           name: 'Interview 7',
+          status: 'Kết thúc',
         },
         {
           id: 8,
           name: 'Interview 8',
+          status: 'Kết thúc',
         },
         {
           id: 9,
           name: 'Interview 9',
+          status: 'Đang diễn ra',
         },
         {
           id: 10,
           name: 'Interview 10',
+          status: 'Chưa bắt đầu',
         },
         {
           id: 11,
           name: 'Interview 11',
+          status: 'Chưa bắt đầu',
         },
         // Thêm các dữ liệu cho các event khác
       ];
       const time = '12/07/2023 10:00';
-      const status = 'Đã đăng ký';
+     
       const itemsPerPage = 10;
       const pathnavigate = '/interview/1';
       const name = 'Interview Name'
       return(
-        <HistoryList events={eventsData} time={time}  status={status} itemsPerPage={itemsPerPage} pathnavigate={pathnavigate} namePage={name}/>
+        <HistoryList events={eventsData} time={time}  itemsPerPage={itemsPerPage} pathnavigate={pathnavigate} namePage={name}/>
       )
       
 }
