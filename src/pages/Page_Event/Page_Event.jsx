27fd411708af70eb60d685d97473190830e9359a
andoan4803@gmail.com <andoan4803@gmail.com>
2023-07-22 11:13:45 +0700

    thaian5

--- src/pages/Page_Event/Page_Event.jsx
+++ src/pages/Page_Event/Page_Event.jsx
@@ -24,91 +24,128 @@ import Pagination from '@mui/material/Pagination';
 const cards1 = [1, 2, 3, 4, 5, 6];
 //
 function Copyright() {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Team 4
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
-  
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Team 4
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
 
 const Page_Event = () => {
   //
   const navigate = useNavigate()
   //
-  
+
   //
-    const handleNavigateClick1 = () => {
-      navigate('/event/:eventid')
-    }
-    return (
-        <>
-        <Box sx={{padding:'24px'}}>
-       
-        <Box 
-        sx=
-        {{
-            backgroundImage:'url(https://bootstrapmade.com/demo/templates/ZenBlog/assets/img/post-landscape-6.jpg)', 
-            height:'300px',
-            borderRadius: '10px'
+  const handleNavigateClick1 = () => {
+    navigate('/event/:eventid')
+  }
+  return (
+    <>
+      <Box sx={{ padding: '24px' }}>
 
-        }}>
-        
-       <Typography variant='h4' align='center' color='white' fontFamily='serif'>
-        Event
-        </Typography>
-      </Box>
-      </Box>
-      
+        <Box
+          sx=
+          {{
+            backgroundImage: 'url(https://png.pngtree.com/background/20210710/original/pngtree-blue-geometric-flattened-texture-banner-background-picture-image_1012881.jpg)',
+            height: '300px',
+            borderRadius: '10px',
+            boxShadow: 5
+          }}>
+          <Box sx={{ padding: '150px 880px 0px 110px' }}>
+            <Typography variant='h3' align='left' color='black' fontFamily='serif' sx={{ borderBottom: '3px solid #0099FF' }}>
+              Event
+            </Typography>
 
-   
-
-
-   <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */
-          <Typography variant='h4' align="left" fontFamily='serif'>View Event</Typography>
-          }
-          <Grid container spacing={4}>
-            {cards1.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '58.25%',
-                    }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Event
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button onClick={handleNavigateClick1} size="small">View</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-          <Box sx={{display:'flex', justifyContent:'flex-end', marginTop:3}}> 
-          <Pagination count={10} variant="outlined" color="primary"  />
           </Box>
-          </Container>
-          {/* Footer */}
-       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+
+          <Box sx={{ padding: '10px 350px 0px 110px' }}>
+            <Typography variant='h6' align='left' color='#808080' fontFamily='serif' >
+              Các sự kiện sôi động sẽ cập nhật liên tục, hãy theo dõi tin tức sự kiện để có thể đăng kí tham gia.
+            </Typography>
+          </Box>
+
+        </Box>
+      </Box>
+
+
+
+
+
+      <Container sx={{ py: 8 }} maxWidth="md">
+        {/* End hero unit */
+          <Box sx={{ padding: '10px 0px 10px 0px', borderTop: '1px solid lightgrey' }}>
+            <Typography variant='h4' align="left" fontFamily='serif'>View Event</Typography>
+          </Box>
+        }
+        <Grid container spacing={4} >
+          {cards1.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: '58.25%',
+                  }}
+                  image="https://greenwich.edu.vn/wp-content/uploads/2023/07/CF23web-940x400.png"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Event
+                  </Typography>
+                  <Box >
+                    <Box sx={{padding:'0px 0px 5px 0px'}}>
+                    <Typography
+                      variant="subtitle2"
+                      align="center"
+                      color="text.secondary"
+                      component="p"
+                      sx={{ display: 'flex', justifyContent: 'flex-start', padding: '0px 0px 5px 0px' }}
+                    >
+                      Sport • July 5th 23
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      align="center"
+                      color="#A0522D"
+
+
+                      sx={{ display: 'flex', justifyContent: 'flex-start', padding: '0px 0px 5px 0px' }}
+                    >
+                      HCM Campus
+                    </Typography>
+                    </Box>
+                   
+
+                    <Typography>
+
+                      [HCM] WorkShop: Motivation at Work
+                    </Typography>
+                  </Box>
+
+                </CardContent>
+                <CardActions >
+                  <Button onClick={handleNavigateClick1} size="small">View</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 3 }}>
+          <Pagination count={10} variant="outlined" color="primary" />
+        </Box>
+      </Container>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
           Team 4
         </Typography>
@@ -124,11 +161,11 @@ const Page_Event = () => {
       </Box>
       {/* End footer */}
 
-        </>
+    </>
 
-       
 
-    )
+
+  )
 }
 
 export default Page_Event;
