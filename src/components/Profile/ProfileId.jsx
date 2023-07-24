aee77a6040dd5e839118c933d9a9af651526c65b
HuyWithNY <huyhmopubg@gmail.com>
2023-07-24 15:22:08 +0700

    edit 24/7

--- src/components/Profile/ProfileId.jsx
+++ src/components/Profile/ProfileId.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
-import { Grid, Typography, Button, Paper } from '@mui/material';
-import { DataGrid ,GridToolbar} from '@mui/x-data-grid';
+import React, { useState, useEffect } from 'react';
+import { Grid, Typography, Button, Paper,Box } from '@mui/material';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom';
 import { NullString, NotStart, Pending, Completed, Postpone } from "../LabelButton/LabelButton";
 import TabInProfile from './TabInProfile/TabInProfile';
+
 export default function HistoryList({ events, time, pathnavigate, namePage }) {
   const [selectedEvent, setSelectedEvent] = useState(null);
   const navigate = useNavigate();
   const drawerWidth = 200;
+  const [totalPositions, setTotalPositions] = useState(0);
+
+  useEffect(() => {
+    // Calculate the total number of positions
+    const total = events.reduce((sum, event) => {
+      if (event.id && Array.isArray(event.id)) {
+        return sum + event.id.length;
+      }
+      return sum;
+    }, 0);
+    setTotalPositions(total);
+  }, [events]);
+
   const handleDetails = (eventId) => {
     navigate(pathnavigate);
     // Thực hiện hành động khi người dùng nhấn vào nút "Xem chi tiết" cho từng event
@@ -59,14 +73,13 @@ export default function HistoryList({ events, time, pathnavigate, namePage }) {
             return <Postpone />;
         }
       },
-
     },
     {
       field: 'view',
       headerName: 'View',
       flex: 1,
       renderCell: (params) => (
-        <Button variant="contained" color="primary" onClick={() => handleDetails(params.row.id)} style={{textTransform:"none"}}>
+        <Button variant="contained" color="primary" onClick={() => handleDetails(params.row.id)} style={{ textTransform: "none" }}>
           Xem chi tiết
         </Button>
       ),
@@ -81,57 +94,50 @@ export default function HistoryList({ events, time, pathnavigate, namePage }) {
   }));
 
   return (
-    <Grid container justifyContent="center">
-      <div>
-        <TabInProfile/>
-      </div>
-      <Grid item xs={12} md={10}> {/* Tăng chiều rộng của khung */}
-      
-        <Paper elevation={3} sx={{ padding: '20px', marginBottom: '20px', width: '100%' }}>
-        <Typography variant="h5" gutterBottom>
-            Application List
+    <>
+      <Grid container direction="column">
+        <Grid item>
+          <Typography variant="h4" gutterBottom>
+            My Story
           </Typography>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            rowStyles={(params) => ({
-              backgroundColor: selectedEvent === params.id ? '#ffdddd' : 'transparent',
-            })}
-            onMouseEnter={(params) => handleEventHover(params.row)}
-            onMouseLeave={handleEventLeave}
-            sx={{ width: '100%' ,
-            "&.MuiDataGrid-root .MuiDataGrid-columnHeader": {
-              backgroundColor: "#1565C0",
-              color: "white",
-              fontWeight: 700,
-              fontSize: 14,
-              border: "none",
-            },
-          }}
-          slots={{ toolbar: GridToolbar }}
-          slotProps={{
-            pagination: {
-              labelRowsPerPage: "Số lượng hiển thị",
-              labelDisplayedRows: ({ from, to, count }) =>
-                `${from}–${to} của ${count !== -1 ? count : `hơn ${to}`}`,
-            },
-            toolbar: {
-              showQuickFilter: true,
-              quickFilterProps: { debounceMs: 500, placeholder: "Tìm kiếm...", sx: {
-                width: 300,
-                marginBottom: 1,
-              }},
-              csvOptions: { disableToolbarButton: true },
-              printOptions: { disableToolbarButton: true }
-          }
-        }} 
-        disableColumnMenu
-        disableColumnFilter
-        disableColumnSelector
-        disableDensitySelector
-          />
-        </Paper>
+        </Grid>
+        <Grid item>
+          <TabInProfile />
+        </Grid>
+        <Grid item xs={12} md={10}>
+          {/* Tăng chiều rộng của khung */}
+          <Paper elevation={3} sx={{ padding: '20px', marginBottom: '20px', width: '100%' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+              <Typography variant="h5" gutterBottom>
+                Application List
+              </Typography>
+              <Box sx={{ backgroundColor: '#dcdcdc', color: 'black', padding: '5px', borderRadius: '10px', marginLeft: '10px' }}>
+                {totalPositions} Application
+              </Box>
+            </Box>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              rowStyles={(params) => ({
+                backgroundColor: selectedEvent === params.id ? '#ffdddd' : 'transparent',
+              })}
+              onMouseEnter={(params) => handleEventHover(params.row)}
+              onMouseLeave={handleEventLeave}
+              sx={{
+                width: '100%',
+                "&.MuiDataGrid-root .MuiDataGrid-columnHeader": {
+                  backgroundColor: "#1565C0",
+                  color: "white",
+                  fontWeight: 700,
+                  fontSize: 14,
+                  border: "none",
+                },
+              }}
+              
+            />
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
--- src/components/Profile/TabInProfile/TabInProfile.jsx
+++ src/components/Profile/TabInProfile/TabInProfile.jsx
@@ -1,17 +1,64 @@
 import React from 'react';
-import { Tabs, Tab } from '@mui/material';
+import { Tabs, Tab, Box } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default function TabInProfile(){
+const theme = createTheme({
+  components: {
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#f8f8ff', // Set the background color of the tabs
+          color: '#d3d3d3', // Set the text color of the tabs
+          textTransform: 'none',
+          marginLeft: 0,
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          fontSize: '16px', // Set the font size of the tabs
+          minWidth: 'auto', // Remove the minimum width to allow tabs to shrink
+          textTransform: 'none',
+          color: '#000000',
+        },
+        textColorInherit: {
+          opacity: 1, // Make the text fully opaque for both selected and non-selected tabs
+        },
+      },
+    },
+  },
+});
+
+export default function TabInProfile() {
   const location = useLocation();
 
   return (
-    <Tabs value={location.pathname}>
-      <Tab label="My Event List" value="/profile/:profileid/event" component={Link} to="/profile/:profileid/event" />
-      <Tab label="My Application List" value="/profile/:profileid/application" component={Link} to="/profile/:profileid/application" />
-      <Tab label="My CVs List" value="/profile/:profileid/cv" component={Link} to="/profile/:profileid/cv" />
-      <Tab label="My Interview List" value="/profile/:profileid/interview" component={Link} to="/profile/:profileid/interview" />
-    </Tabs>
+    <ThemeProvider theme={theme}>
+      <Box mt={3} mb={5}> {/* Add margin (10px) on top and bottom */}
+        <Tabs value={location.pathname}>
+          <Tab
+            label="My Event List"
+            value="/profile/:profileid/event"
+            component={Link}
+            to="/profile/:profileid/event"
+            
+          />
+          <Tab
+            label="My Application List"
+            value="/profile/:profileid/application"
+            component={Link}
+            to="/profile/:profileid/application"
+          />
+          <Tab
+            label="My Interview List"
+            value="/profile/:profileid/interview"
+            component={Link}
+            to="/profile/:profileid/interview"
+          />
+        </Tabs>
+      </Box>
+    </ThemeProvider>
   );
-};
-
+}
