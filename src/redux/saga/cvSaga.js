1a77161d749032a7d55e6b16b4c9fbb6ebc8973f
namkha1032 <namkha1032@gmail.com>
2023-07-25 14:05:55 +0700

    update host

--- src/redux/saga/cvSaga.js
+++ src/redux/saga/cvSaga.js
@@ -1,17 +1,17 @@
 // import libraries
 import { takeEvery, put, all, call, takeLatest } from "redux-saga/effects"
 import axios from 'axios'
-
+import host from "../host"
 function* getCv(action) {
-    const response1 = yield call(axios.get, 'http://localhost:3000/data/cvid.json')
-    const response2 = yield call(axios.get, `http://localhost:3000/data/candidateid.json?candidateid=${response1.data.userid}`)
+    const response1 = yield call(axios.get, `${host.name}/data/cvid.json`)
+    const response2 = yield call(axios.get, `${host.name}/data/candidateid.json?candidateid=${response1.data.userid}`)
     // yield put({ type: "saga/getCandidate", payload: response1.data.userid })
     yield put({ type: "cv/setCv", payload: response1.data })
     yield put({ type: "candidate/setCandidate", payload: response2.data })
 }
 
 function* getCvList(action) {
-    const reponse = yield call(axios.get, 'http://localhost:3000/data/CVList.json')
+    const reponse = yield call(axios.get, `${host.name}/data/CVList.json`)
     yield put({ type: 'cvlist/setCvList', payload: reponse.data })
 }
 
--- src/redux/saga/questionSaga.js
+++ src/redux/saga/questionSaga.js
@@ -2,18 +2,18 @@
 import { takeEvery, put, all, call, takeLatest } from "redux-saga/effects"
 import axios from 'axios'
 import { delay } from "../../utils/delay"
-
+import host from "../host"
 function* getAllQuestion() {
     yield put({type: "loading/onLoading"})
     yield call(delay, 1500)
-    const response = yield call(axios.get, 'http://localhost:3000/data/questionlist.json')
+    const response = yield call(axios.get, `${host.name}/data/questionlist.json`)
     yield put({ type: "questionList/setQuestionList", payload: response.data })
     console.log(response.data)
     yield put({type: "loading/offLoading"})
 }
 
 function* getInterviewQuestion(action) {
-    const response = yield call(axios.get, 'http://localhost:3000/data/questionlist.json')
+    const response = yield call(axios.get, `${host.name}/data/questionlist.json`)
     yield put({ type: "question/setInterviewQuestion", payload: response.data })
 }
 
