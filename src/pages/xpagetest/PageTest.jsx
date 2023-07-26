1e34142505bf50cb9f41cb03295c8aa28c40f113
namkha1032 <namkha1032@gmail.com>
2023-07-26 17:46:30 +0700

    switch to my branch

--- src/pages/xpagetest/PageTest.jsx
+++ src/pages/xpagetest/PageTest.jsx
@@ -3,24 +3,7 @@ import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 const PageTest = () => {
     const dispatch = useDispatch()
-    useEffect(() => {
-        const newPost = {
-            title: "post ban hang online",
-            author: "namkha",
-            comments: [
-                {
-                    body: "lol cmt 1"
-                },
-                {
-                    body: "lol cmt 2"
-                },
-                {
-                    body: "lol cmt 3"
-                }
-            ]
-        }
-        dispatch({ type: "saga/getStuff", payload: newPost })
-    }, [])
+    
     return (
         <>
         </>
