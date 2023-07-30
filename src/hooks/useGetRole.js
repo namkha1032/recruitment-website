d022f241fd435670f0fea8d09cd440f26b16f1e6
Mercury-2910 <bao.dang2910022hcmut.edu.vn>
2023-07-30 20:22:33 +0700

    fix UI

--- src/hooks/useGetRole.js
+++ src/hooks/useGetRole.js
@@ -13,30 +13,83 @@ import host from "../redux/host";
 // -------------------README !!!---------------------------
 
 
+// function useGetRole() {
+//     const userlocal = useSelector(state => state.user)
+//     console.log("user: ", userlocal)
+//     // const userlocal = JSON.parse(window.localStorage.getItem("user"))
+//     const [role, setRole] = useState(null)
+//     useEffect(() => {
+//         if (userlocal) {
+
+//             let token = `Bearer ${userlocal.token}`
+//             const config = {
+//                 headers: { Authorization: token },
+//             }
+//             axios.get('https://leetun2k2-001-site1.gtempurl.com/api/Authentication/CurrentRole', config).then(response => {
+//                 console.log("response is: ", response.data.role[0])
+//                 if (response.data.role[0] == "Admin") {
+//                     setRole("admin")
+//                 }
+//                 else if (response.data.role[0] == "Recruiter") {
+//                     setRole("recruiter")
+//                 }
+//                 else if (response.data.role[0] == "Interviewer") {
+//                     setRole("interviewer")
+//                 }
+//                 else if (response.data.role[0] == "Candidate") {
+//                     setRole("candidate")
+//                 }
+//                 else {
+//                     setRole(null)
+//                 }
+//             })
+//         }
+//         else {
+//             setRole(null)
+//         }
+        
+//     }, [userlocal])
+//     return role
+// }
+
+// export default useGetRole
+
+
 function useGetRole() {
-    const userlocal = useSelector(state => state.user)
-    console.log("user: ", userlocal)
-    // const userlocal = JSON.parse(window.localStorage.getItem("user"))
+    const user = useSelector(state => state.user)
     const [role, setRole] = useState(null)
     useEffect(() => {
-        if (userlocal) {
-
-            let token = `Bearer ${userlocal.token}`
+        if (user) {
+            // -----------------------------------------------------------
+            let api = ""
+            if (user.token == "hentai.jav.admin") {
+                api = `${host.name}/data/roleAdmin.json`
+            }
+            else if (user.token == "hentai.jav.recruiter") {
+                api = `${host.name}/data/roleRecruiter.json`
+            }
+            else if (user.token == "hentai.jav.interviewer") {
+                api = `${host.name}/data/roleInterviewer.json`
+            }
+            else if (user.token == "hentai.jav.candidate") {
+                api = `${host.name}/data/roleCandidate.json`
+            }
+            // -----------------------------------------------------------
+            let token = `Bearer ${user.token}`
             const config = {
                 headers: { Authorization: token },
             }
-            axios.get('https://leetun2k2-001-site1.gtempurl.com/api/Authentication/CurrentRole', config).then(response => {
-                console.log("response is: ", response.data.role[0])
-                if (response.data.role[0] == "Admin") {
+            axios.get(api, config).then(response => {
+                if (response.data.roleName == "admin") {
                     setRole("admin")
                 }
-                else if (response.data.role[0] == "Recruiter") {
+                else if (response.data.roleName == "recruiter") {
                     setRole("recruiter")
                 }
-                else if (response.data.role[0] == "Interviewer") {
+                else if (response.data.roleName == "interviewer") {
                     setRole("interviewer")
                 }
-                else if (response.data.role[0] == "Candidate") {
+                else if (response.data.roleName == "candidate") {
                     setRole("candidate")
                 }
                 else {
@@ -47,12 +100,12 @@ function useGetRole() {
         else {
             setRole(null)
         }
-        
-    }, [userlocal])
+    }, [user])
     return role
 }
 
 export default useGetRole
+
 
 
 
--- src/redux/saga/userSaga.js
+++ src/redux/saga/userSaga.js
@@ -2,38 +2,79 @@ import { takeEvery, put, all, call, takeLatest } from "redux-saga/effects"
 import axios from 'axios'
 import host from "../host"
 
+// function* userLogin(action) {
+//     try {
+//         const { username, password } = action.payload
+//         const response = yield call(axios.post, 'https://leetun2k2-001-site1.gtempurl.com/api/Authentication/Login', { username, password })
+//         console.log("response is: ", response.data)
+
+//         yield put({ type: "user/setUser", payload: response.data })
+//         window.localStorage.setItem("user", JSON.stringify(response.data))
+
+//         let userlocal = JSON.parse(window.localStorage.getItem("user"))
+//         let token = `Bearer ${userlocal.token}`
+//         console.log(token)
+//         const config = {
+//             headers: { Authorization: token },
+//         }
+//         const userId = yield call(axios.get, 'https://leetun2k2-001-site1.gtempurl.com/api/Authentication/CurrentUser', config)
+//         console.log("userId is", userId.data)
+//         yield put({ type: "user/setUser", payload: userId.data })
+//         yield put({ type: "error/setError", payload: { status: "no", message: "" } })
+
+//     }
+//     catch (error) {
+//         yield put({ type: "error/setError", payload: { status: "yes", message: error.response.data.error } })
+//         console.log("err: ", error.response.data.status)
+//     }
+// }
+
 function* userLogin(action) {
     try {
-        const { username, password } = action.payload
-        const response = yield call(axios.post, 'https://leetun2k2-001-site1.gtempurl.com/api/Authentication/Login', { username, password })
-        console.log("response is: ", response.data)
-
-        yield put({ type: "user/setUser", payload: response.data })
-        window.localStorage.setItem("user", JSON.stringify(response.data))
-
-        let userlocal = JSON.parse(window.localStorage.getItem("user"))
-        let token = `Bearer ${userlocal.token}`
-        console.log(token)
-        const config = {
-            headers: { Authorization: token },
+        /* const response = yield call(axios.post, 'http://localhost:3001/api/login', action.payload)
+        yield call(window.localStorage.setItem, 'user', JSON.stringify(response.data))
+        yield put({ type: "user/userLogin", payload: response.data }) */
+        const { username, password, check } = action.payload
+        let api = ""
+        if (username == "candidate1" && password == "candidate1") {
+            api = `${host.name}/data/userCandidate.json`
         }
-        const userId = yield call(axios.get, 'https://leetun2k2-001-site1.gtempurl.com/api/Authentication/CurrentUser', config)
-        console.log("userId is", userId.data)
-        yield put({ type: "user/setUser", payload: userId.data })
-        /* if (check) {
+        else if (username == "interviewer1" && password == "interviewer1") {
+            api = `${host.name}/data/userInterviewer.json`
+        }
+        else if (username == "recruiter1" && password == "recruiter1") {
+            api = `${host.name}/data/userRecruiter.json`
+        }
+        else if (username == "admin1" && password == "admin1") {
+            api = `${host.name}/data/userAdmin.json`
+        }
+        else {
+            throw {
+                response: {
+                    data: {
+                        error: "username or password is incorrect"
+                    }
+                }
+            }
+        }
+        const response = yield call(axios.get, api)
+        yield put({ type: "user/setUser", payload: response.data })
+        if (check) {
             window.localStorage.setItem("user", JSON.stringify(response.data))
         }
         else {
             window.sessionStorage.setItem("user", JSON.stringify(response.data))
-        } */
+        }
         yield put({ type: "error/setError", payload: { status: "no", message: "" } })
 
     }
     catch (error) {
         yield put({ type: "error/setError", payload: { status: "yes", message: error.response.data.error } })
-        console.log("err: ", error.response.data.status)
+        console.log("err: ", error)
     }
 }
+
+
 
 function* userRegister(action) {
     try {
