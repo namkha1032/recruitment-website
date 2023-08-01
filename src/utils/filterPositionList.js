5ffd81878f17dd8a3665b5dd35bba8c80db5b33e
unknown <khoa.caotrananh@hcmut.edu.vn>
2023-08-01 15:34:06 +0700

    Update api GET

--- src/utils/filterPositionList.js
+++ src/utils/filterPositionList.js
@@ -11,7 +11,7 @@ export function filterPositionList(input, filter) {
         return input
     }
     else if (filter.status === null) {
-        return input.filter(element => element.departmentId === filter.departmentId)
+        return input.filter(element => element.department.departmentId === filter.departmentId)
     }
     else if (filter.departmentId === null) {
         if (filter.status === "Active") {
@@ -23,10 +23,10 @@ export function filterPositionList(input, filter) {
     }
     else {
         if (filter.status === "Active") {
-            return input.filter(element => element.isDeleted === false && element.departmentId === filter.departmentId)
+            return input.filter(element => element.isDeleted === false && element.department.departmentId === filter.departmentId)
         }
         else {
-            return input.filter(element => element.isDeleted === true && element.departmentId === filter.departmentId)
+            return input.filter(element => element.isDeleted === true && element.department.departmentId === filter.departmentId)
         }
     }
 }
