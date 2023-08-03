f3a27166dae58d4a2d8e2d43a6a4ed8e18c2ad6f
huy291002 <nqh29102002@gmail.com>
2023-08-03 09:25:41 +0700

    fix saga

--- src/pages/Page_Recruitment_Id/Page_Recruitment_Id.jsx
+++ src/pages/Page_Recruitment_Id/Page_Recruitment_Id.jsx
@@ -112,7 +112,7 @@ const Page_Recruitment_Id = () => {
             }
             else {
                 dispatch({
-                    type: 'submitsaga/submitCv',
+                    type: 'applicationSaga/submitCv',
                     payload: {
                         cvId: CV,
                         positionId: recruitmentid,
